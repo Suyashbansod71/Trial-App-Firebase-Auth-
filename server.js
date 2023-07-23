c5e526c96bb2e82f9d7daede3a015ec42a7d4c3e
Suyash Bansod <77398556+Suyashbansod71@users.noreply.github.com>
2023-07-23 18:51:18 +0530

    updated code

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan  = require("morgan");
+const colors = require("colors");
+const dotnev = require("dotenv");
+const path = require("path");
+
+//dotenv config
+dotnev.config();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.get("/", (req,res) => {
+
+    res.status(200).send({
+       message:"server running",
+    });
+});
+
+
+//static files
+app.use(expres.static(path.join(__dirname, "./client/build")));
+
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+});
+
+//port
+const port = process.env.PORT || 5000
+
+//listen post
+app.listen(port, () => {
+  console.log(`Server Running on port ${process.env.PORT}`
+  .bgCyan.white
+  );
+});
